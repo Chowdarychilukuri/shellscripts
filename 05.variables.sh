97876b13e068c9a9264eb5b03e4403338537cbdc
chowdary chilukuri <152151632+Chowdarychilukuri@users.noreply.github.com>
2023-12-13 22:02:19 +0530

    updating code

--- 05.variables.sh
+++ 05.variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+echo "Please enter your Username: "
+read USERNAME  #the value entered above will be automatically attached to USERNAME variable
 
-echo "username is: $USERNAME, Password is: $PASSWORD"
+echo "Please enter your Password: "
+read -s PASSWORD
+
+echo "username is: $USERNAME, Password is: $PASSWORD"  #I am priniting just for validations, you should not print it in real time
